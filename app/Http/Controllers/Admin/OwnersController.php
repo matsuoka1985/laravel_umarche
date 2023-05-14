ab62a9e10d3c78a21bc24e59c31d29bf77efd6c5
shingo matsuoka <shingomatsuoka@shingonoMacBook-Pro.local>
2023-05-14 11:01:28 +0900

    -store

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Models\Owner;
 use Illuminate\Support\Facades\DB; //クエリビルダ
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Hash;
+
 
 class OwnersController extends Controller
 {
@@ -56,7 +58,24 @@ class OwnersController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // $request->name;
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:owners'],
+            'password' => ['required', 'confirmed', ],
+        ]);
+
+        Owner::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+
+
+        return redirect()->route('admin.owners.index')
+        ->with('message','オーナー登録を実施しました');
+
     }
 
     /**
