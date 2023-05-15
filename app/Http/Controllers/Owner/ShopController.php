04dceede0f616a23354d886c79c69e8a57963e70
shingo matsuoka <shingomatsuoka@shingonoMacBook-Pro.local>
2023-05-15 18:26:23 +0900

    リサイズなしのアップロード

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Shop;
+use Illuminate\Support\Facades\Storage;
 
 class ShopController extends Controller
 {
@@ -37,11 +38,16 @@ class ShopController extends Controller
     }
 
     public function edit($id){
-        dd(Shop::findOrFail($id));
-
+        // dd(Shop::findOrFail($id));
+        $shop=Shop::findOrFail($id);
+        return view('owner.shops.edit',compact('shop'));
     }
 
     public function update(Request $request,$id){
-
+        $imageFile=$request->image;
+        if(!is_null($imageFile) && $imageFile->isValid() ){
+            Storage::putFile('public/shops',$imageFile); // storage/app/public/shops以下に保存するという意味。
+        }
+        return redirect()->route('owner.shops.index');
     }
 }
