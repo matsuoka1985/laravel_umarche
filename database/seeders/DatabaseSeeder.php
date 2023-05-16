a6e8972c4e70af38ce896f6824e033f145e79a4a
shingo matsuoka <shingomatsuoka@shingonoMacBook-Pro.local>
2023-05-17 08:59:01 +0900

    category

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
            OwnerSeeder::class,
            ShopSeeder::class,
            ImageSeeder::class,
+           CategorySeeder::class
         ]);
-
     }
 }
