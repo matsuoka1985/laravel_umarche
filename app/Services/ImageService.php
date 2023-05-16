628e050f24da9951828705336cb6246418bf78e1
shingo matsuoka <shingomatsuoka@shingonoMacBook-Pro.local>
2023-05-16 20:56:32 +0900

    store

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -9,10 +9,16 @@ class ImageService{
      * 画像ファイルそのものをstorage/app/public/{任意のディレクトリ}に保存し、その画像のファイル名を保存パスも含め戻り値として返す関数。
      */
     public static function upload($imageFile,$folderName){
+        // dd($imageFile['image']);
+        if(is_array($imageFile)){
+            $file=$imageFile['image'];
+        }else{
+            $file=$imageFile;
+        }
         $fileName = (string)date('Y-m-d H:i:s') . uniqid(rand() . '_'); //ファイル名を生成。
-        $extension = $imageFile->extension(); //アップロードされたファイルから拡張子を取得。
+        $extension = $file->extension(); //アップロードされたファイルから拡張子を取得。
         $fileNameToStore = $fileName . '.' . $extension; // 生成したファイル名に拡張子を追加。
-        $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode(); //アップロードされたファイルをリサイズ。
+        $resizedImage = InterventionImage::make($file)->resize(1920, 1080)->encode(); //アップロードされたファイルをリサイズ。
         Storage::put(
             'public/' . $folderName . '/'  . $fileNameToStore,
              $resizedImage); //　(パスも含め)第一引数のファイル名によって、第二引数のファイルを保存。
