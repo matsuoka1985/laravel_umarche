5129f2d53e11042a1a6e4c6dfa4e355310055636
shingo matsuoka <shingomatsuoka@shingonoMacBook-Pro.local>
2023-05-17 22:19:47 +0900

    product create

--- resources/views/owner/products/create.blade.php
+++ resources/views/owner/products/create.blade.php
@@ -18,7 +18,61 @@
                         <div class=" -m-2">
                             <div class="p-2 w-1/2 mx-auto">
                                 <div class="relative">
-                                    <select name="category" >
+                                    <label for="name" class="leading-7 text-sm text-gray-600">商品名 *必須</label>
+                                    <input type="text" id="name" name="name" required
+                                        value="{{ old('name') }}"
+                                        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                </div>
+                            </div>
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                    <label for="information" class="leading-7 text-sm text-gray-600">商品情報 *必須</label>
+                                    <textarea id="information" name="information" required rows="10"
+                                        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">{{ old('information') }}</textarea>
+                                </div>
+                            </div>
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                    <label for="price" class="leading-7 text-sm text-gray-600">価格 *必須</label>
+                                    <input type="number" id="price" name="price" required
+                                        value="{{ old('price') }}"
+                                        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                </div>
+                            </div>
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                    <label for="sort_order" class="leading-7 text-sm text-gray-600">表示順</label>
+                                    <input type="number" id="sort_order" name="sort_order"
+                                        value="{{ old('sort_order') }}"
+                                        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                </div>
+                            </div>
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                    <label for="quantity" class="leading-7 text-sm text-gray-600">初期在庫 * 必須</label>
+                                    <input type="number" id="quantity" name="quantity" required
+                                        value="{{ old('quantity') }}"
+                                        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                </div>
+                            </div>
+
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                                                        <label for="shop_id" id="shop_id" class="leading-7 text-sm text-gray-600">販売する店舗</label>
+
+                                    <select name="shop_id" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" >
+                                        @foreach ($shops as $shop)
+                                            <option value="{{$shop->id}}">
+                                                {{$shop->name}}
+                                            </option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                    <label for="category" id="category" class="leading-7 text-sm text-gray-600">カテゴリー</label>
+                                    <select name="category" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                         @foreach ($categories as $category)
                                             <optgroup label="{{$category->name}}">
                                                 @foreach ($category->secondary as $secondary)
@@ -29,6 +83,28 @@
                                             </optgroup>
                                         @endforeach
                                     </select>
+                                </div>
+                            </div>
+
+                            <x-select-image :images="$images" name="image1" />
+                            <x-select-image :images="$images" name="image2" />
+                            <x-select-image :images="$images" name="image3" />
+                            <x-select-image :images="$images" name="image4" />
+                            <div class="hidden">
+                                <x-select-image :images="$images" name="image5" />
+                            </div>
+
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative flex justify-around">
+
+                                    <div>
+                                        <input type="radio" name="is_selling" value="1" class="mr-2"
+                                            checked >販売中
+                                    </div>
+                                    <div>
+                                        <input type="radio" name="is_selling" value="0" class="mr-2"
+                                            >停止中
+                                    </div>
                                 </div>
                             </div>
 
@@ -46,4 +122,22 @@
             </div>
         </div>
     </div>
+<script>
+"use strict";
+const images = document.querySelectorAll(".image"); //クラス名にimageとついているものを全て取得
+images.forEach(image => { //上で取得したタグ全てのループで回す。
+    image.addEventListener('click', function (e) {
+        const imageName = e.target.dataset.id.substr(0, 6);
+        const imageId = e.target.dataset.id.replace(imageName + '_', '');
+        const imageFile = e.target.dataset.file;
+        const imagePath = e.target.dataset.path;
+        const modal = e.target.dataset.modal;
+
+        document.getElementById(imageName + '_thumbnail').src = imagePath + '/' + imageFile;
+        document.getElementById(imageName + '_hidden').value = imageId
+        MicroModal.close(modal);
+    });
+});
+
+</script>
 </x-app-layout>
