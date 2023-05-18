9dadcf343e14552486becc3c8c5101fd38cecde2
shingo matsuoka <shingomatsuoka@shingonoMacBook-Pro.local>
2023-05-18 22:33:58 +0900

    route,itemcontroller

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ComponentTestController;
 use App\Http\Controllers\LifeCycleTestController;
+use App\Http\Controllers\User\ItemController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,18 +16,22 @@ use App\Http\Controllers\LifeCycleTestController;
 |
 */
 
-
-
-Route::get('/a', function () {
-    return "やれやれだぜ";
-});
 Route::get('/', function () {
     return view('user.welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('user.dashboard');
-})->middleware(['auth:users'])->name('dashboard');
+Route::middleware('auth:users')->group(function () {
+    Route::get('/', [ItemController::class, 'index'])->name('items.index');
+});
+
+
+
+
+
+
+// Route::get('/dashboard', function () {
+//     return view('user.dashboard');
+// })->middleware(['auth:users'])->name('dashboard');
 
 Route::get('/component-test1', [ComponentTestController::class, 'showComponent1']);
 Route::get('/component-test2', [ComponentTestController::class, 'showComponent2']);
