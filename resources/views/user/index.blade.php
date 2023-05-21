c7643924b8a10af5fa893dc39e9f9a66670cfa69
shingo matsuoka <shingomatsuoka@shingonoMacBook-Pro.local>
2023-05-21 13:35:37 +0900

    show調整

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -14,7 +14,7 @@
                             @foreach ($products as $product)
                                 <div class="w-1/4 p-2 md:p-4">
                                     <a href="
-                                    {{-- {{ route('owner.products.edit', ['product' => $product->id]) }} --}}
+                                    {{ route('user.items.show', ['item' => $product->id]) }}
                                     ">
                                         <div class="border rounded-md p-2 md:p-4">
                                             <x-thumbnail filename="{{$product->filename ?? ''}}" type="products" />
