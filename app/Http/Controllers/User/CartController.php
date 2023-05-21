d78a26b69e4c86c3ee56ad60182bdd03b451d0d0
shingo matsuoka <shingomatsuoka@shingonoMacBook-Pro.local>
2023-05-21 22:18:42 +0900

    stripe前の在庫処理

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Cart;
 use App\Models\User;
+use App\Models\Stock;
 use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
@@ -59,6 +60,13 @@ class CartController extends Controller
 
         $lineItems=[];
         foreach($products as $product){
+            $quantity='';
+            $quantity=Stock::where('product_id',$product->id)->sum('quantity');
+
+            if($product->pivot->quantity>$quantity){
+                return redirect()->route('user.cart.index');
+            }
+
             $lineItem=[
                 'name'=>$product->name,
                 'description'=>$product->information,
@@ -68,6 +76,19 @@ class CartController extends Controller
             ];
             $lineItems[]=$lineItem;
         }
+
+        foreach($products as $product){
+            //Stockテーブルは商品を追加したり減らしたりするときに使用する。
+            Stock::create([
+                'product_id'=>$product->id,
+                'type'=>\Constant::PRODUCT_LIST['reduce'],
+                'quantity'=>$product->pivot->quantity * -1
+            ]);
+        }
+
+        dd('test');
+
+
         // dd($lineItems);
         \Stripe\Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
 
