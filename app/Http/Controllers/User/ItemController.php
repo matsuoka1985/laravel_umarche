0cedef97935bf0326588e1628d637566530d8cba
shingo matsuoka <shingomatsuoka@shingonoMacBook-Pro.local>
2023-05-22 12:43:43 +0900

    コンストラクタの修正

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -7,11 +7,25 @@ use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Stock;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class ItemController extends Controller
 {
     public function __construct(){
         $this->middleware('auth:users');
+
+        $this->middleware(function ($request, $next) {
+
+            $id = $request->route()->parameter('item');
+            if (!is_null($id)) {
+                $itemId = Product::availableItems()->where('products.id',$id)->exists();
+                if (!$itemId) {
+                    abort(404);
+                }
+            }
+
+            return $next($request);
+        });
     }
 
     public function index(){
